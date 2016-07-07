4c7da0d223a4d76bcfd3652c6da68fe4b586dd67
xopherdeep <heylisten@xtiv.net>
2016-07-06 20:08:09 -0700

    xSponsors/ Automated Nightly Push

---
+++ xSponsors.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @name    Sponsors
+ * @desc
+ * @version 0.1
+ * @author  @xopherdeep
+ * @icon    flag
+ * @mini    flag
+ * @see     radius
+ * @link    sponsors
+ * @todo
+ * @alpha   true
+ * @release alpha
+ */
+
+class xSponsors extends Xengine{
+
+}
+
+?>
